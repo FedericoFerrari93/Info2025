37dee1128fdb0b48ce9a23204267b18fccb09e86
matiasmierezsuarez <matiassamu5@gmail.com>
2025-12-04 22:00:03 -0300

    actualizo vistas de registro y login de usuario

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 from .forms import RegistroUsuarioForm
 
 class RegistroUsuario(CreateView):
     form_class = RegistroUsuarioForm
     template_name = 'usuarios/registro.html'
     success_url = reverse_lazy('apps.usuario:login') # Redirige al login tras registrarse
+
+class Login(auth_views.LoginView):
+    template_name = 'usuarios/login.html'
